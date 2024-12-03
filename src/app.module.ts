3afd3e8e808211139f37c8cb8431b4238324e709
Saeed Norouzi <saeednorouzi98@gmail.com>
2024-12-03 12:32:12 +0330

    Feat(WalletModule): Add wallet module
    Description:
    Initialize an empty nestjs module which will be used to handle
    wallet-related processes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
+import { WalletModule } from './modules/wallet/wallet.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, WalletModule],
   controllers: [],
   providers: [],
 })
