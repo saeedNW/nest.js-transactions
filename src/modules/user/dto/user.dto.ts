81ee18b4fe1c40219ff91c8b07cfb25dae375c4d
Saeed Norouzi <saeednorouzi98@gmail.com>
2024-12-04 17:05:41 +0330

    Fix(UserModule): Fix create user dto
    Description:
    Add validation conditions to the dto

--- src/modules/user/dto/user.dto.ts
+++ src/modules/user/dto/user.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateUserDto {
+	@IsString()
+	@IsNotEmpty()
 	phone: string;
+	@IsString()
+	@IsNotEmpty()
 	fullName: string;
 }
